a43c52267f184b8f296e6bff2dd334522880dbee
Justin Brulotte <justin.brlotte797@gmail.com>
2019-04-02 15:30:02 -0400

    Adding Adapter for RabbitMQ

--- contrib/rabbitmq/pkg/adapter/adapter.go
+++ contrib/rabbitmq/pkg/adapter/adapter.go
@@ -14,12 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package kafka
+package rabbitmq
 
 import (
-	"encoding/json"
 	"strconv"
-	"strings"
 
 	"github.com/Shopify/sarama"
 	"github.com/cloudevents/sdk-go/pkg/cloudevents"
@@ -27,12 +25,13 @@ import (
 	"github.com/cloudevents/sdk-go/pkg/cloudevents/types"
 	"github.com/knative/eventing-sources/pkg/kncloudevents"
 	"github.com/knative/pkg/logging"
+	"github.com/streadway/amqp"
 	"go.uber.org/zap"
 	"golang.org/x/net/context"
 )
 
 const (
-	eventType = "dev.knative.kafka.event"
+	eventType = "dev.knative.rabbitmq.event"
 )
 
 type AdapterSASL struct {
@@ -41,6 +40,7 @@ type AdapterSASL struct {
 	Password string
 }
 
+// TODO we should have a system to use that in the futur
 type AdapterTLS struct {
 	Enable bool
 }
@@ -51,43 +51,32 @@ type AdapterNet struct {
 }
 
 type Adapter struct {
+
+	// BootstrapServers is the RabbitMQ Broker URL that we're polling messages from
 	BootstrapServers string
-	Topics           string
-	ConsumerGroup    string
-	Net              AdapterNet
-	SinkURI          string
-	client           client.Client
+
+	// RabbitMQ Exchange name
+	ExchangeName string
+
+	// SinkURI is the URI messages will be forwarded on to.
+	SinkURI string
+
+	// Creds to connect to RabbitMQ
+	Creds AdapterNet
+
+	// Client sends cloudevents to the target.
+	client client.Client
 }
 
 // --------------------------------------------------------------------
 
-// ConsumerGroupHandler functions to define message consume and related logic.
-func (a *Adapter) Setup(_ sarama.ConsumerGroupSession) error {
+// Initialize cloudevent client
+func (a *Adapter) initClient() error {
 	if a.client == nil {
 		var err error
 		if a.client, err = kncloudevents.NewDefaultClient(a.SinkURI); err != nil {
 			return err
 		}
-	}
-	return nil
-}
-func (a *Adapter) Cleanup(_ sarama.ConsumerGroupSession) error { return nil }
-func (a *Adapter) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-
-	logger := logging.FromContext(context.TODO())
-
-	for msg := range claim.Messages() {
-		logger.Info("Received: ", zap.Any("value", string(msg.Value)))
-
-		go func(msg *sarama.ConsumerMessage) {
-			// send and mark message if post was successful
-			if err := a.postMessage(context.TODO(), msg); err == nil {
-				sess.MarkMessage(msg, "")
-				logger.Debug("Successfully sent event to sink")
-			} else {
-				logger.Error("Sending event to sink failed: ", zap.Error(err))
-			}
-		}(msg)
 	}
 	return nil
 }
@@ -99,68 +88,104 @@ func (a *Adapter) Start(ctx context.Context, stopCh <-chan struct{}) error {
 
 	logger.Info("Starting with config: ", zap.Any("adapter", a))
 
-	kafkaConfig := sarama.NewConfig()
-	kafkaConfig.Consumer.Offsets.Initial = sarama.OffsetOldest
-	kafkaConfig.Version = sarama.V2_0_0_0
-	kafkaConfig.Consumer.Return.Errors = true
-	kafkaConfig.Net.SASL.Enable = a.Net.SASL.Enable
-	kafkaConfig.Net.SASL.User = a.Net.SASL.User
-	kafkaConfig.Net.SASL.Password = a.Net.SASL.Password
-	kafkaConfig.Net.TLS.Enable = a.Net.TLS.Enable
-
-	// Start with a client
-	client, err := sarama.NewClient(strings.Split(a.BootstrapServers, ","), kafkaConfig)
-	if err != nil {
-		panic(err)
+	if err := a.initClient(); err != nil {
+		logger.Error("Failed to create cloudevent client", zap.Error(err))
+		return err
 	}
-	defer func() { _ = client.Close() }()
 
-	// init consumer group
-	group, err := sarama.NewConsumerGroupFromClient(a.ConsumerGroup, client)
-	if err != nil {
-		panic(err)
+	// Connect to RabbitMQ Brocker
+	// TODO we should check if SASL is enable
+	amqpConnection := a.BootstrapServers + a.Creds.SASL.User + ":" + a.Creds.SASL.Password + "@rabbitmq/"
+	if conn, err := amqp.Dial(amqpConnection); err != nil {
+		logger.Error("Failed to connect to RabbitMQ", zap.Error(err))
+		return err
 	}
-	defer func() { _ = group.Close() }()
+	defer conn.Close()
 
-	// Track errors
-	go func() {
-		for err := range group.Errors() {
-			logger.Error("ERROR", err)
-		}
-	}()
+	// Create a channel with the brocker
+	if ch, err := conn.Channel(); err != nil {
+		logger.Error("Failed to open a channel", zap.Error(err))
+		return err
+	}
+	defer ch.Close()
 
-	// Handle session
-	go func() {
-		for {
-			if err := group.Consume(ctx, strings.Split(a.Topics, ","), a); err != nil {
-				panic(err)
-			}
-		}
-	}()
+	if err = ch.ExchangeDeclare(
+		a.ExchangeName, // name
+		"direct",       // type
+		true,           // durable
+		false,          // auto-deleted
+		false,          // internal
+		false,          // no-wait
+		nil,            // arguments
+	); err != nil {
+		logger.Error("Failed to declare an exchange", zap.Error(err))
+		return err
+	}
+
+	if q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	); err != nil {
+		logger.Error("Failed to declare an queue", zap.Error(err))
+		return err
+	}
+
+	return a.pollLoop(ctx, ch, q, stopCh)
+}
+
+// pollLoop continuously polls from the given RabbitMQ queue until stopCh
+// emits an element.  The
+func (a *Adapter) pollLoop(ctx context.Context, ch *amqp.Channel, q *amqp.Queue, stopCh <-chan struct{}) error {
+
+	logger := logging.FromContext(ctx)
+
+	if msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto ack
+		false,  // exclusive
+		false,  // no local
+		false,  // no wait
+		nil,    // args
+	); err != nil {
+		logger.Error("Failed to register a consumer", zap.Error(err))
+		return err
+	}
 
 	for {
 		select {
 		case <-stopCh:
-			logger.Info("Shutting down...")
+			logger.Info("Exiting")
 			return nil
+		default:
+		}
+		for m := range msgs {
+			/*  Message structure form the queue
+			Owner:         m.ReplyTo      		--> Producer URL if needed
+			Body:          m.Body				--> Message Body
+			CorrelationID: m.CorrelationId		--> Unique ID of the message if needed
+
+			TODO We should also have a critical message system if there is a error
+			*/
+			a.receiveMessage(ctx, m)
 		}
 	}
 }
 
-func (a *Adapter) postMessage(ctx context.Context, msg *sarama.ConsumerMessage) error {
-
-	extensions := map[string]interface{}{
-		"key": string(msg.Key),
-	}
+func (a *Adapter) postMessage(ctx context.Context, logger *zap.SugaredLogger, msg *amqp.Delivery) error {
+	// TODO We have to change that for CloudEvent
 	event := cloudevents.Event{
 		Context: cloudevents.EventContextV02{
 			SpecVersion: cloudevents.CloudEventsVersionV02,
 			Type:        eventType,
-			ID:          "partition:" + strconv.Itoa(int(msg.Partition)) + "/offset:" + strconv.FormatInt(msg.Offset, 10),
+			ID:          m.CorrelationId,
 			Time:        &types.Timestamp{Time: msg.Timestamp},
-			Source:      *types.ParseURLRef(msg.Topic),
-			ContentType: cloudevents.StringOfApplicationJSON(),
-			Extensions:  extensions,
+			Source:      *types.ParseURLRef(msg.Exchange), // TODO not very sure here
+			ContentType: cloudevents.StringOfApplicationJSON()
 		}.AsV02(),
 		Data: a.jsonEncode(ctx, msg.Value),
 	}
@@ -169,15 +194,19 @@ func (a *Adapter) postMessage(ctx context.Context, msg *sarama.ConsumerMessage) 
 	return err
 }
 
-func (a *Adapter) jsonEncode(ctx context.Context, value []byte) interface{} {
-	var payload map[string]interface{}
+// receiveMessage handles an incoming message from the RabbitMQ queue,
+// and forwards it to a Sink, calling `Delivery.Ack()` when the forwarding is
+// successful.
+func (a *Adapter) receiveMessage(ctx context.Context, m *amqp.Delivery) {
+	logger := logging.FromContext(ctx).With(zap.Any("eventID", m.MessageId)).With(zap.Any("sink", a.SinkURI))
+	logger.Debugw("Received message from RabbitMQ:", zap.Any("message", string(m.Body)))
 
-	logger := logging.FromContext(ctx)
-
-	if err := json.Unmarshal(value, &payload); err != nil {
-		logger.Info("Error unmarshalling JSON: ", zap.Error(err))
-		return value
+	err := a.postMessage(ctx, logger, m)
+	if err != nil {
+		logger.Infof("Event delivery failed: %s", err)
+		m.Nack(false, true) // TODO Implement priority system for message that can be drop and system that need to be requeue
 	} else {
-		return payload
+		logger.Debug("Message successfully posted to Sink")
+		m.Ack(false)
 	}
 }
